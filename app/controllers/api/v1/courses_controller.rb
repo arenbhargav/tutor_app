d8e2bcc78e65e75b3cfa74365507c673628cb46e
Arvind Bhargav <arenbhargav@gmail.com>
2023-06-22 22:06:17 +0530

    Add serializer

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::CoursesController < ApplicationController
+  def index
+    courses = Course.includes(:tutors)
+
+    render json: courses, each_serializer: ::CourseSerializer
+  end
+
   def create
     course = Course.new(course_params)
 
     if course.save
-      return render json: course, status: :created
+      return render json: course, each_serializer: ::CourseSerializer, status: :created
     end
 
-    render json: { errors: course.errors.full_messages },
-            status: :unprocessable_entity 
+    render json: { errors: course.errors.full_messages }, status: :unprocessable_entity 
   end
 
   private
--- app/controllers/api/v1/tutors_controller.rb
+++ app/controllers/api/v1/tutors_controller.rb
@@ -2,18 +2,15 @@ class Api::V1::TutorsController < ApplicationController
   def create
     tutor = Tutor.new(tutor_params)
     if tutor.save
-      return render json: tutor, status: :created
+      render json: tutor, each_serializer: ::TutorSerializer, status: :created
     end
 
-    render json: { errors: tutor.errors.full_messages },
-            status: :unprocessable_entity
+    render json: { errors: tutor.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
 
   def tutor_params
-    params.permit(
-      :avatar, :name, :username, :email, :password, :password_confirmation
-    )
+    params.permit(:avatar, :name, :username, :email, :password, :password_confirmation)
   end
 end
